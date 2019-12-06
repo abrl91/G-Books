48f4b1bbaa88aa1f55ba8602eba9b5f24f3c9574
Amit Barletz <barletzamit@Amits-MBP.lan>
2019-12-07 01:37:21 +0200

    folder strucrure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './features/welcome-page/welcome-page.component';
+import { MainComponent } from './features/main/main.component';
+import { WishListComponent } from './features/wish-list/wish-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BookComponent } from './shared/book/book.component';
+import { PopupComponent } from './shared/popup/popup.component';
+import { PaginatorComponent } from './shared/paginator/paginator.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    MainComponent,
+    WishListComponent,
+    HeaderComponent,
+    BookComponent,
+    PopupComponent,
+    PaginatorComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
